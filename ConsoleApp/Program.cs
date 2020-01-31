246b814cf3c21d1f338688ced13d1068069d7e81
Maks <maxim.tatsenko@gmail.com>
2020-01-31 20:53:12 +0200

    Add thumb

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using WmiParser;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser p = new Parser();
+            p.Parse("", 1);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
